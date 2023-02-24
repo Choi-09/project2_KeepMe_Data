98a6099245fb621080cbe43a6696a571b65d261e
jaewooths <thswodn1234@naver.com>
2023-02-24 15:11:42 +0900

    [230224] FEAT: SQS 메시지를 flask를통해 보내고 DB저장

--- 3.flask/pybo/__init__.py
+++ 3.flask/pybo/__init__.py
@@ -4,6 +4,8 @@ import boto3
 import json
 import mysql.connector
 import uuid
+import random
+import time
 
 # MySQL 데이터베이스 설정
 mysql_host = 'localhost'
@@ -22,6 +24,33 @@ conn = mysql.connector.connect(user=mysql_user, password=mysql_password,
 cursor = conn.cursor(prepared=True)
 
 
+def send_sqs_messages():
+    while True:
+        # GPS Table의 랜덤 더미 데이터 생성
+        gps_data = {
+            'id': str(random.randint(1, 100)),
+            'user_id': str(random.randint(1, 10)),
+            'latitude': random.uniform(-90, 90),
+            'longitude': random.uniform(-180, 180),
+            'time_sent': str(random.randint(1, 10))
+        }
+        message_body = 'Hello'
+        message_deduplication_id = str(uuid.uuid4())
+
+        # GPS 데이터 SQS에 전송
+        response = sqs.send_message(
+            QueueUrl=queue_url,
+            MessageBody=json.dumps(gps_data),
+            MessageGroupId='my-group' + gps_data['id'],
+            MessageDeduplicationId=message_deduplication_id,
+        )
+
+        print('Sent GPS data: ', gps_data)
+
+        # # 10초 대기
+        time.sleep(3)
+
+
 def save_sqs_messages_to_mysql():
     while True:
         # SQS에서 메시지를 받아옴
@@ -38,7 +67,7 @@ def save_sqs_messages_to_mysql():
                 # 메시지 데이터 추출
                 message_data = json.loads(message['Body'])
 
-                # model
+                # model 처리
 
                 # MySQL DB에 데이터 저장
                 sql = "INSERT INTO gps (id, user_id, latitude, longitude, time_sent) VALUES (%s, %s, %s, %s, %s)"
@@ -59,6 +88,13 @@ def create_app():
 
     from .views import main_views
     app.register_blueprint(main_views.bp)
+    app.run(host='127.0.0.1', port=5000)
 # 메시지 저장 스레드 시작
-    threading.Thread(target=save_sqs_messages_to_mysql).start()
+    t1 = threading.Thread(target=send_sqs_messages)
+    t2 = threading.Thread(target=save_sqs_messages_to_mysql)
+
+    t1.daemon = True
+    t2.daemon = True
+    t1.start()
+    t2.start()
     return app
